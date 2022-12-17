d1166cc7ab24038185486d2ebc314c9022ac2e78
Borza <boris.zagar@gmail.com>
2022-12-17 20:53:22 +0100

    Day 4 Problem 1 and Problem 2

---
+++ Day 4/Problem 1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int lineNumber = 1;
+int overLapCount = 0;
+foreach(var line in File.ReadLines(@"Data\inputdata.txt"))
+{
+    if (OverLaps(line))
+    { 
+        overLapCount++;
+        Console.WriteLine($"Line: {lineNumber}. {line} overlaps");
+    }
+    lineNumber++;
+}
+
+Console.WriteLine($"There are totally {overLapCount} overlaps.");
+
+static bool OverLaps(string parts)
+{
+    bool result = false;
+
+    var ranges = parts.Split(',');
+    var range1 = new Range(ranges[0]);
+    var range2 = new Range(ranges[1]);
+
+    result = range1.Overlaps(range2) || range2.Overlaps(range1);
+
+    return result;
+}
+
+readonly struct Range
+{
+    public readonly int start;
+    public readonly int end;
+
+    public Range(string startEnd)
+    {
+        var parts = startEnd.Split('-');
+        start = int.Parse(parts[0]);
+        end = int.Parse(parts[1]);
+    }
+
+    internal bool Overlaps(Range rh)
+    {
+        return rh.start <= start && rh.end >= end;
+    }
+}
