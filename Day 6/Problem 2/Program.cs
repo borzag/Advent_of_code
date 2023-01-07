d9ae21db49b688e7f8f3c8ebce3d16da9d294306
Borza <boris.zagar@gmail.com>
2023-01-08 00:14:50 +0100

    Day 6 Problem 1 and 2

---
+++ Day 6/Problem 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var data = File.ReadAllText(@"Data\inputdata.txt").ToArray();
+
+var currentSequence = new Queue<char>(data.Take(13));
+for (int i = 13; i < data.Length; i++)
+{
+    currentSequence.Enqueue(data[i]);
+    if (IsUniqueSequence(currentSequence))
+    {
+        Console.WriteLine($"Unique sequence found at position '{i + 1}'");
+        break;
+    }
+    currentSequence.Dequeue();
+}
+
+bool IsUniqueSequence(Queue<char> currentSequence)
+{
+    foreach (var item in currentSequence)
+    {
+        if (currentSequence.Count(x => x.Equals(item)) > 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
